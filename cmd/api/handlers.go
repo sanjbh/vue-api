a67c850bb707c6fb6241b60316bfe5f66328d872
Sanjay Bhattacharya <sanjbh@gmail.com>
2024-01-21 17:52:21 +0530

    added chi

---
+++ cmd/api/handlers.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+}
+
+func (app *application) Login(w http.ResponseWriter, r *http.Request) {
+	type credentials struct {
+		UserName string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	var creds credentials
+	var payload jsonResponse
+
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		app.errorLog.Printf("Invalid json: %v\n", err)
+		payload.Error = true
+		payload.Message = "Invalid JSON"
+
+		out, err := json.MarshalIndent(payload, "", "\t")
+		if err != nil {
+			app.errorLog.Println(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(out)
+		return
+	}
+
+	//authenticate
+	app.infoLog.Println(creds.UserName, creds.Password)
+	//send back a response
+	payload.Error = false
+	payload.Message = "Signed In"
+	out, err := json.MarshalIndent(payload, "", "\t")
+	if err != nil {
+		app.errorLog.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
+}
